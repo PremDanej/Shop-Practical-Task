611ba1d69edd844fe61fc01de0e25090d1655ecb
PremDanej <danejprem005@gmail.com>
2025-05-26 17:30:32 +0530

    Theme, Color and Typography updated.

--- app/src/main/java/com/merp/jet/shop/practical/app/ui/theme/Type.kt
+++ app/src/main/java/com/merp/jet/shop/practical/app/ui/theme/Type.kt
@@ -1,34 +1,37 @@
 package com.merp.jet.shop.practical.app.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.LineHeightStyle
+import com.merp.jet.shop.practical.app.R
+
+val CenturyFont = Font(R.font.century_bold)
+val StandardFont = Font(R.font.neuzeits_book)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val Typography = Typography().run {
+    val fontFamily = FontFamily(StandardFont)
+    val lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+    copy(
+        displayLarge = displayLarge.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        displayMedium = displayMedium.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        displaySmall = displaySmall.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        headlineLarge = headlineLarge.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        headlineMedium = headlineMedium.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        headlineSmall = headlineSmall.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        titleLarge = titleLarge.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        titleMedium = titleMedium.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        titleSmall = titleSmall.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        bodyLarge = bodyLarge.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        bodyMedium = bodyMedium.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        bodySmall = bodySmall.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        labelLarge = labelLarge.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        labelMedium = labelMedium.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
+        labelSmall = labelSmall.copy(fontFamily = fontFamily, lineHeightStyle = lineHeightStyle),
     )
-    */
-)
+}
